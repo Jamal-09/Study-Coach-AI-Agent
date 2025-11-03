5edd8af993524df9182e32d66dca1ea04f098843
Jamal <napg.adekunle@gmail.com>
2025-11-03 18:40:06 +0100

    feat: Added a2a route and tsconfig.json

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,6 +1,21 @@
 import { Mastra } from "@mastra/core/mastra";
 import { studyCoachAgent } from "./agents/studyCoach-agent.js";
+import { a2aAgentRoute } from "../mastra/routes/studyCoach-route.js";
 
 export const mastra = new Mastra({
   agents: { studyCoachAgent },
+  telemetry: {
+    enabled: false,
+  },
+  observability: {
+    default: { enabled: true },
+  },
+
+  server: {
+    build: {
+      openAPIDocs: true,
+      swaggerUI: true,
+    },
+    apiRoutes: [a2aAgentRoute],
+  },
 });
