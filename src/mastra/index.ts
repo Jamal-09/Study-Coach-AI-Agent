c8639f2d7d5982cc439a9b143c9b8a70fad4a1dc
Jamal <napg.adekunle@gmail.com>
2025-11-03 23:23:01 +0100

    fix: Fixed a2a route for proper configurations

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,21 +1,20 @@
-import { Mastra } from "@mastra/core/mastra";
-import { studyCoachAgent } from "./agents/studyCoach-agent.js";
-import { a2aAgentRoute } from "../mastra/routes/studyCoach-route.js";
-
+import { LibSQLStore } from "@mastra/libsql";
+import { PinoLogger } from "@mastra/loggers";
+import { studyCoachAgent } from "./agents/studyCoach-agent";
+import { Mastra, OtelConfig } from "@mastra/core";
+import { a2aAgentRoute } from "./routes/studyCoach-route";
 export const mastra = new Mastra({
-  agents: { studyCoachAgent },
   telemetry: {
-    enabled: false,
-  },
-  observability: {
-    default: { enabled: true },
-  },
-
+    serviceName: "feedbackAgent", // required
+    // optionally add exporter config, sampling, etc.
+  } as OtelConfig,
+  agents: { studyCoachAgent },
+  storage: new LibSQLStore({ url: ":memory:" }),
+  logger: new PinoLogger({ name: "Mastra", level: "debug" }),
+  observability: { default: { enabled: true } },
   server: {
-    build: {
-      openAPIDocs: true,
-      swaggerUI: true,
-    },
+    build: { openAPIDocs: true, swaggerUI: true },
     apiRoutes: [a2aAgentRoute],
   },
 });
+console.log("hi", process.env.GOOGLE_GENERATIVE_AI_API_KEY);
